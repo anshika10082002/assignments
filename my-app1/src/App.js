75302f773c93d5a40820a86e7da6119c67ae37c7
anshika10082002 <anshika10082002@gmail.com>
2023-01-05 11:16:24 +0530

    done

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,35 +1,57 @@
 import React from "react";
 import "./index.css";
 
-class TextArea extends React.Component {
+class Form extends React.Component {
     constructor(){
     super()
         this.state={
-         input:'',
-         showParagraph:false
+         fname:'',
+         lname:''
+         
         }
     }
 
     render(){
       return (
-      <div class="div">
-            <textarea
-             placeholder="enter text here...."
-            value={this.state.input}
-             onChange={(event)=> this.setState({input:event.target.value})}/>
+      <div>
+            <form>
+              <label for="fname">First Name</label>
+              <br/>
+              <input type="text" 
+              value={this.state.fname}
+               onChange={(event)=>this.state({fname:event.target.value})}/><br/>
+              <label for="lname">Last Name</label>
+              <br/>
+              <input type="text"
+              value={this.state.lname}
+              onChange={(event)=>this.state({lname:event.target.value})}
+              /><br/>
+              <label for="gender">Gender</label>
+              <br/>
+              <input type="radio"/>Male<br/>
+              <input type="radio"/>Female<br/>
+
+              <label for="country">Country</label><br/>
+              <select>
+                <option>India</option>
+                <option>America</option>
+                <option>Australia</option>
+                <option>Canada</option>
+                <option>Korea</option>
+              </select>
+             </form>
              <br/>
              <button
              onClick={() => {
                 this.setState({
-                  showParagraph: !this.state.showParagraph,
+                  
                 });
               }}
-             >Change into UpperCase</button>
-             {this.state.showParagraph && <p>{this.state.input.toUpperCase()}
-             </p>}
+             >Submit</button>
            
         </div>)
     }
 }
 
-export default TextArea;
+export default  Form ;
+  
--- my-app1/src/index.js
+++ my-app1/src/index.js
@@ -1,34 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import  TextArea from "./App"
+import  Form from "./App"
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// //function component
-// const HeadingComponent  =  ()  =>{
-//   return <h1>Hello World</h1>
-
-// }
-// //class component
-// class HeadingComponentUsingClass extends React.Component{
-//     render(){
-//       return <h1>Hello world again</h1>
-//     }
-// }
-
 
 root.render(
   <React.StrictMode>
-    <TextArea/>
+    <Form/>
   
   </React.StrictMode>
 );
 
-// {/* <HeadingComponent/>
- //  <HeadingComponentUsingClass/> */}
+
 
 
 
